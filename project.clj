eff1ab62eb42940f9a5267cadadfb59177345c4a
Dan Larkin <dan@danlarkin.org>
2010-02-07 18:13:51 -0500

    update project.clj to reflect dependency on clojure 1.2

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject org.danlarkin/clojure-json "1.1-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
